619ac7ba0c4979d685b78f3d2c6594730e9d66b4
Marco Kiefer <kiefer.marco@me.com>
2018-02-27 10:32:32 +0100

    Klasse Parkplatz erstellt & neue Testklasse erstellt

---
+++ Parkplatz/src/de/gbsschulen/parkplatz/Parkplatz.java
@@ -0,0 +1,19 @@
+package de.gbsschulen.parkplatz;
+
+public class Parkplatz {
+    protected Auto[] parkplatz;
+    protected int anzahlAutos;
+
+    public Parkplatz() {
+        this.parkplatz = new Auto[3];
+        this.anzahlAutos = 0;
+    }
+    public Auto parken(Auto auto){
+        // TODO parken implementieren
+        return null;
+    }
+    public Auto wegfahren(Auto auto){
+        // TODO wegfahren implementieren
+        return null;
+    }
+}
